c3d9c03290bd502a21bff6b139f9813e118bf51d
Wangjialin <wangjialin@espressif.com>
2017-01-11 15:40:46 +0800

    first commit with solution framework

---
+++ component.mk
@@ -0,0 +1,9 @@
+#
+# Component Makefile
+#
+COMPONENT_ADD_INCLUDEDIRS := components include abstract
+
+COMPONENT_SRCDIRS := main library port
+
+#CFLAGS += -Wno-unused-function -DHAVE_EXPAT_CONFIG_H
+
