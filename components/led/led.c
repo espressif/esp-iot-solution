fa4e1da133a5124d9d268669f0616e338f064de4
qianyicheng <qianyichen@espressif.com>
2017-03-13 18:58:25 +0800

    feat(led) : make it able to set duty in night mode

--- components/led/led.c
+++ components/led/led.c
@@ -1,17 +1,26 @@
-// Copyright 2015-2016 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+/*
+  * ESPRESSIF MIT License
+  *
+  * Copyright (c) 2017 <ESPRESSIF SYSTEMS (SHANGHAI) PTE LTD>
+  *
+  * Permission is hereby granted for use on ESPRESSIF SYSTEMS products only, in which case,
+  * it is free of charge, to any person obtaining a copy of this software and associated
+  * documentation files (the "Software"), to deal in the Software without restriction, including
+  * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+  * and/or sell copies of the Software, and to permit persons to whom the Software is furnished
+  * to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in all copies or
+  * substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  *
+  */
 #include <stdio.h>
 #include "driver/ledc.h"
 #include "freertos/FreeRTOS.h"
@@ -30,6 +39,7 @@
 #define LED_LONG_DARK_CHANNEL       LEDC_CHANNEL_1
 #define LED_QUICK_BLINK_CHANNEL     LEDC_CHANNEL_2
 #define LED_SLOW_BLINK_CHANNEL      LEDC_CHANNEL_3
+#define LED_NIGHT_MODE_CHANNEL      LEDC_CHANNEL_4
 
 #define LED_NORMAL_TIMER   LEDC_TIMER_0
 #define LED_QUICK_BLINK_TIMER   LEDC_TIMER_1
@@ -47,6 +57,7 @@
 #define RES_ASSERT(tag, res, ret)   IOT_CHECK(tag, (res) != pdFALSE, ret)
 
 static const char* TAG = "LED";
+uint8_t g_night_duty = 0;
 typedef struct {
     uint8_t io_num;
     led_status_t state;
@@ -85,8 +96,14 @@ static esp_err_t led_level_set(led_handle_t led_handle, uint8_t level)
 {
     led_dev_t* led_dev = (led_dev_t*) led_handle;
     if ((level & 0x01) ^ led_dev->dark_level) {
-        ERR_ASSERT(TAG, gpio_ledc_bind(LED_NORMAL_TIMER, LED_LONG_BRIGHT_CHANNEL, led_dev->io_num, 
-                    LED_BRIGHT_DUTY, LED_SPEED_MODE));
+        if (led_dev->mode == LED_NIGHT_MODE) {
+            ERR_ASSERT(TAG, gpio_ledc_bind(LED_NORMAL_TIMER, LED_NIGHT_MODE_CHANNEL, led_dev->io_num, 
+                        g_night_duty * LED_BRIGHT_DUTY / 100, LED_SPEED_MODE));
+        }
+        else {
+            ERR_ASSERT(TAG, gpio_ledc_bind(LED_NORMAL_TIMER, LED_LONG_BRIGHT_CHANNEL, led_dev->io_num, 
+                        LED_BRIGHT_DUTY, LED_SPEED_MODE));
+        }
     }
     else {
         ERR_ASSERT(TAG, gpio_ledc_bind(LED_NORMAL_TIMER, LED_LONG_DARK_CHANNEL, led_dev->io_num, 
@@ -141,25 +158,20 @@ esp_err_t led_state_write(led_handle_t led_handle, led_status_t state)
 {
     led_dev_t* led_dev = (led_dev_t*) led_handle;
     POINT_ASSERT(TAG, led_dev);
-    if (led_dev->mode == LED_NIGHT_MODE) {
-        led_level_set(led_handle, 0);
-    }
-    else {
-        switch (state) {
-            case LED_NORMAL_OFF:
-                led_level_set(led_handle, 0);
-                break;
-            case LED_NORMAL_ON:
-                led_level_set(led_handle, 1);
-                break;
-            case LED_QUICK_BLINK:
-                led_quick_blink(led_handle);
-                break;
-            case LED_SLOW_BLINK:
-                led_slow_blink(led_handle);
-            default:
-                break;
-        }
+    switch (state) {
+        case LED_NORMAL_OFF:
+            led_level_set(led_handle, 0);
+            break;
+        case LED_NORMAL_ON:
+            led_level_set(led_handle, 1);
+            break;
+        case LED_QUICK_BLINK:
+            led_quick_blink(led_handle);
+            break;
+        case LED_SLOW_BLINK:
+            led_slow_blink(led_handle);
+        default:
+            break;
     }
     led_dev->state = state;
     return ESP_OK;
@@ -172,6 +184,18 @@ esp_err_t led_mode_write(led_handle_t led_handle, led_mode_t mode)
     led_dev->mode = mode;
     led_state_write(led_handle, led_dev->state);
     return ESP_OK;
+}
+
+esp_err_t led_night_duty_write(uint8_t duty)
+{
+    IOT_CHECK(TAG, duty <= 100, ESP_FAIL);
+    g_night_duty = duty;
+    return ESP_OK;
+}
+
+uint8_t led_night_duty_read()
+{
+    return g_night_duty;
 }
 
 led_status_t led_state_read(led_handle_t led_handle)
