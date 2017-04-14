37b23b7e003ef2db540def3b487474db5278e086
qianyicheng <qianyichen@espressif.com>
2017-04-14 17:49:34 +0800

    bugfix(power meter): change error check to avoid memory leak because of not suitable
    1. change error check to avoid memory leak because of not suitable
    2. all the meter pins of the same powermeter share the same timer, and the timer won't be started continually in pcnt intr

--- components/power_meter/test/power_meter_test.c
+++ components/power_meter/test/power_meter_test.c
@@ -14,11 +14,12 @@
 #define PM_CURRENT_PARAM    13670
 
 static const char* TAG = "powermeter_test";
+pm_handle_t pm_handle;
 
 static void read_value(void* arg)
 {
     pm_handle_t pm_handle = arg;
-    while (1) {
+    for (int i = 0; i < 2; i++) {
         powermeter_change_mode(pm_handle, PM_SINGLE_VOLTAGE);
         vTaskDelay(5000 / portTICK_RATE_MS);
         ESP_LOGI(TAG, "value of power:%d", powermeter_read(pm_handle, PM_POWER));
@@ -29,29 +30,13 @@ static void read_value(void* arg)
         ESP_LOGI(TAG, "value of power:%d", powermeter_read(pm_handle, PM_POWER));
         ESP_LOGI(TAG, "value of voltage:%d", powermeter_read(pm_handle, PM_VOLTAGE));
         ESP_LOGI(TAG, "value of current:%d", powermeter_read(pm_handle, PM_CURRENT));
-        powermeter_delete(pm_handle);
-        vTaskDelay(5000 / portTICK_RATE_MS);
-        pm_config_t pm_conf = {
-            .power_io_num = PM_CF_IO_NUM,
-            .power_pcnt_unit = PCNT_UNIT_0,
-            .power_ref_param = PM_POWER_PARAM,
-            .voltage_io_num = PM_CFI_IO_NUM,
-            .voltage_pcnt_unit = PCNT_UNIT_1,
-            .voltage_ref_param = PM_VOLTAGE_PARAM,
-            .current_io_num = PM_CFI_IO_NUM,
-            .current_pcnt_unit = PCNT_UNIT_1,
-            .current_ref_param = PM_CURRENT_PARAM,
-            .sel_io_num = 17,
-            .sel_level = 0,
-            .pm_mode = PM_SINGLE_VOLTAGE
-        };
-        pm_handle = powermeter_create(pm_conf);
     }
     vTaskDelete(NULL);
 }
 
 void power_meter_test()
 {
+    printf("before power meter create, heap: %d\n", esp_get_free_heap_size());
     pm_config_t pm_conf = {
         .power_io_num = PM_CF_IO_NUM,
         .power_pcnt_unit = PCNT_UNIT_0,
@@ -66,6 +51,9 @@ void power_meter_test()
         .sel_level = 0,
         .pm_mode = PM_SINGLE_VOLTAGE
     };
-    pm_handle_t pm_handle = powermeter_create(pm_conf);
+    pm_handle = powermeter_create(pm_conf);
     xTaskCreate(read_value, "read_value", 2048, pm_handle, 5, NULL);
+    vTaskDelay(60000 / portTICK_RATE_MS);
+    powermeter_delete(pm_handle);
+    printf("after power meter delete, heap: %d\n", esp_get_free_heap_size());
 }
