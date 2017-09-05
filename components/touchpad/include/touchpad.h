7ba9556086fa73d2e04bac74b97b61d335867047
qianyicheng <qianyicheng@espressif.com>
2017-09-05 17:01:54 +0800

    style(touchpad): add comments to touchpad component, change class names, add CProximitySensor and CGestureSensor

--- components/touchpad/include/touchpad.h
+++ components/touchpad/include/touchpad.h
@@ -37,8 +37,8 @@ typedef void* touchpad_matrix_handle_t;
 typedef void* proximity_sensor_handle_t;
 typedef void* gesture_sensor_handle_t;
 
-typedef void (* touchpad_cb)(void *);
-typedef void (* touchpad_matrix_cb)(void *, uint8_t, uint8_t);
+typedef void (* touchpad_cb)(void *);           /**< callback function of touchpad */
+typedef void (* touchpad_matrix_cb)(void *, uint8_t, uint8_t);      /**< callback function of touchpad matrix */
 
 typedef enum {
     TOUCHPAD_CB_PUSH = 0,        /**< touch pad push callback */
@@ -48,22 +48,22 @@ typedef enum {
 } touchpad_cb_type_t;
 
 typedef enum {
-    PROXIMITY_IDLE = 0,
-    PROXIMITY_APPROACHING,
-    PROXIMITY_LEAVING,
+    PROXIMITY_IDLE = 0,         /**< idle status of proximity sensor */
+    PROXIMITY_APPROACHING,      /**< approaching status of proximity sensor */
+    PROXIMITY_LEAVING,          /**< leaving status of proximity sensor */
 } proximity_status_t;
 
-typedef void (* proximity_cb)(void *, proximity_status_t);
+typedef void (* proximity_cb)(void *, proximity_status_t);      /**< callback function of proximity sensor */
 
 typedef enum {
-    GESTURE_TOP_TO_BOTTOM = 0,
-    GESTURE_BOTTOM_TO_TOP,
-    GESTURE_LEFT_TO_RIGHT,
-    GESTURE_RIGHT_TO_LEFT,
+    GESTURE_TOP_TO_BOTTOM = 0,      /**< gesture from top of sensor to bottom of sensor */
+    GESTURE_BOTTOM_TO_TOP,          /**< gesture from bottom of sensor to top of sensor */
+    GESTURE_LEFT_TO_RIGHT,          /**< gesture from left of sensor to right of sensor */
+    GESTURE_RIGHT_TO_LEFT,          /**< gesture from right of sensor to left of sensor */
     GESTURE_TYPE_MAX,
 } gesture_type_t;
 
-typedef void (* gesture_cb)(void *, gesture_type_t);
+typedef void (* gesture_cb)(void *, gesture_type_t);            /**< callback function fo gesture sensor */
 /**
   * @brief  create touchpad device
   *
@@ -367,51 +367,297 @@ esp_err_t gesture_sensor_add_cb(gesture_sensor_handle_t gest_hd, gesture_cb cb, 
 #endif
 
 #ifdef __cplusplus
-class touchpad
+#define DEFAULT_THRES_PERCENT   950
+#define DEFAULT_FILTER_MS       100
+
+/**
+ * class of touchpad
+ */
+class CTouchPad
 {
 private:
     touchpad_handle_t m_tp_handle;
-    touchpad(const touchpad &);
-    touchpad& operator = (const touchpad &);
-public:
-    touchpad(touch_pad_t touch_pad_num, uint16_t thres_percent, uint32_t filter_value);
-    ~touchpad();
 
+    /**
+     * prevent copy construct
+     */
+    CTouchPad(const CTouchPad &);
+    CTouchPad& operator = (const CTouchPad &);
+public:
+    /**
+      * @brief  constructor of CTouchPad
+      *
+      * @param  touch_pad_num refer to struct touch_pad_t
+      * @param  thres_percent touchpad would be active if the sample value decay to thres_percent 1/1000
+      * @param  filter_value filter time of touchpad
+      */
+    CTouchPad(touch_pad_t touch_pad_num, uint16_t thres_percent = DEFAULT_THRES_PERCENT, uint32_t filter_value = DEFAULT_FILTER_MS);
+    
+    ~CTouchPad();
+
+    /**
+      * @brief  add callback function
+      *
+      * @param  cb_type the type of callback to be added
+      * @param  cb the callback function
+      * @param  arg the argument of callback function
+      *
+      * @return
+      *     - ESP_OK: succeed
+      *     - ESP_FAIL: fail
+      */
     esp_err_t add_cb(touchpad_cb_type_t cb_type, touchpad_cb cb, void  *arg);
+
+    /**
+      * @brief  set serial tigger
+      *
+      * @param  trigger_thres_sec serial event would trigger after trigger_thres_sec seconds
+      * @param  interval_ms evetn would trigger every interval_ms
+      * @param  cb the callback function
+      * @param  arg the argument of callback function
+      *
+      * @return
+      *     - ESP_OK: succeed
+      *     - ESP_FAIL: fail
+      */
     esp_err_t set_serial_trigger(uint32_t trigger_thres_sec, uint32_t interval_ms, touchpad_cb cb, void *arg);
+    
+    /**
+      * @brief  add custom callback function
+      *
+      * @param  press_sec the callback function would be called pointed seconds
+      * @param  cb the callback function
+      * @param  arg the argument of callback function
+      *
+      * @return
+      *     - ESP_OK: succeed
+      *     - ESP_FAIL: fail
+      */
     esp_err_t add_custom_cb(uint32_t press_sec, touchpad_cb cb, void  *arg);
+
+    /**
+      * @brief  get the number of a touchpad
+      *
+      * @return  touchpad number
+      */
     touch_pad_t touchpad_num();
+
+    /**
+      * @brief  set the threshold of touchpad
+      *
+      * @param  threshold value
+      *
+      * @return
+      *     - ESP_OK: succeed
+      *     - ESP_FAIL: the param touchpad_handle is NULL
+      */
     esp_err_t set_threshold(uint32_t threshold);
+
+    /**
+      * @brief  set the filter value of touchpad
+      *
+      * @param  filter_value
+      *
+      * @return
+      *     - ESP_OK: succeed
+      *     - ESP_FAIL: the param touchpad_handle is NULL
+      */
     esp_err_t set_filter(uint32_t filter_value);
+
+    /**
+      * @brief  get sample value of the touchpad
+      *
+      * @return sample value
+      */
     uint16_t value();
 };
 
-class touchpad_slide
+/**
+ * class of touchpad slide
+ */
+class CTouchPadSlide
 {
 private:
     touchpad_slide_handle_t m_tp_slide_handle;
-    touchpad_slide(const touchpad_slide &);
-    touchpad_slide& operator = (const touchpad_slide &);
-public:
-    touchpad_slide(uint8_t num, const touch_pad_t *tps, uint32_t pos_scale, uint16_t thres_percent, uint32_t filter_value);
-    ~touchpad_slide();
 
+    /**
+     * prevent copy construct
+     */
+    CTouchPadSlide(const CTouchPadSlide &);
+    CTouchPadSlide& operator = (const CTouchPadSlide &);
+public:
+    /**
+      * @brief  constructor of CTouchPadSlide
+      *
+      * @param  num number of touchpads the slide uses
+      * @param  tps the array of touchpad num
+      * @param  pos_scale the position scale of each pad
+      * @param  thres_percent touchpad would be active if the sample value decay to thres_percent
+      * @param  filter_value filter time of touchpad
+      */
+    CTouchPadSlide(uint8_t num, const touch_pad_t *tps, uint32_t pos_scale = 2, uint16_t threshold = DEFAULT_THRES_PERCENT, uint32_t filter_value = DEFAULT_FILTER_MS);
+    
+    ~CTouchPadSlide();
+
+    /**
+      * @brief  get relative position
+      *
+      * @return  relative position of your touch on slide
+      */
     uint8_t get_position();
 };
 
-class touchpad_matrix
+/**
+ * class of touchpad matrix
+ */
+class CTouchPadMatrix
 {
 private:
     touchpad_matrix_handle_t m_tp_matrix;
-    touchpad_matrix(const touchpad_matrix &);
-    touchpad_matrix& operator = (const touchpad_matrix &);
-public:
-    touchpad_matrix(uint8_t x_num, uint8_t y_num, const touch_pad_t *x_tps, const touch_pad_t *y_tps, uint16_t thres_percent, uint32_t filter_value);
-    ~touchpad_matrix();
 
+    /**
+     * prevent copy construct
+     */
+    CTouchPadMatrix(const CTouchPadMatrix &);
+    CTouchPadMatrix& operator = (const CTouchPadMatrix &);
+public:
+    /**
+      * @brief  constructor of CTouchPadMatrix
+      *
+      * @param  x_num number of touch sensor on x axis
+      * @param  y_num number of touch sensor on y axis
+      * @param  x_tps the array of touch sensor num on x axis
+      * @param  y_tps the array of touch sensor num on y axis
+      * @param  thres_percent touchpad would be active if the sample value decay to thres_percent
+      * @param  filter_value filter time of touchpad
+      *
+      */
+    CTouchPadMatrix(uint8_t x_num, uint8_t y_num, const touch_pad_t *x_tps, const touch_pad_t *y_tps, uint16_t threshold = DEFAULT_THRES_PERCENT, uint32_t filter_value = DEFAULT_FILTER_MS);
+    
+    ~CTouchPadMatrix();
+
+    /**
+      * @brief  add callback function
+      *
+      * @param  cb_type the type of callback to be added
+      * @param  cb the callback function
+      * @param  arg the argument of callback function
+      *
+      * @return
+      *     - ESP_OK: succeed
+      *     - ESP_FAIL: fail
+      */
     esp_err_t add_cb(touchpad_cb_type_t cb_type, touchpad_matrix_cb cb, void *arg);
+
+    /**
+      * @brief  add custom callback function
+      *
+      * @param  press_sec the callback function would be called after pointed seconds
+      * @param  cb the callback function
+      * @param  arg the argument of callback function
+      *
+      * @return
+      *     - ESP_OK: succeed
+      *     - ESP_FAIL: fail
+      */
     esp_err_t add_custom_cb(uint32_t press_sec, touchpad_matrix_cb cb, void *arg);
+
+    /**
+      * @brief  set serial tigger
+      *
+      * @param  trigger_thres_sec serial event would trigger after trigger_thres_sec seconds
+      * @param  interval_ms evetn would trigger every interval_ms
+      * @param  cb the callback function
+      * @param  arg the argument of callback function
+      *
+      * @return
+      *     - ESP_OK: succeed
+      *     - ESP_FAIL: fail
+      */
     esp_err_t set_serial_trigger(uint32_t trigger_thres_sec, uint32_t interval_ms, touchpad_matrix_cb cb, void *arg);
+};
+
+/**
+ * class of proximity sensor
+ */
+class CProximitySensor
+{
+private:
+    proximity_sensor_handle_t m_prox_sensor;
+
+    /**
+     * prevent copy construct
+     */
+    CProximitySensor(const CProximitySensor &);
+    CProximitySensor& operator = (const CProximitySensor &);
+public:
+    /**
+      * @brief  constructor of CProximitySensor
+      *
+      * @param  tp number of touch sensor
+      * @param  thres_percent touchpad would be active if the sample value decay to thres_percent
+      * @param  filter_value filter time of touchpad
+      */
+    CProximitySensor(touch_pad_t tp, uint16_t thres_percent = 990, uint32_t filter_value = DEFAULT_FILTER_MS);
+    
+    ~CProximitySensor();
+
+    /**
+      * @brief  add callback function
+      *
+      * @param  cb
+      * @param  arg
+      *
+      * @return
+      *     - ESP_OK: succeed
+      *     - others: fail
+      */
+    esp_err_t add_cb(proximity_cb cb, void *arg);
+
+    /**
+      * @brief  get relative value of proximity sensor
+      *
+      * @return  relative value
+      */
+    uint16_t value();
+};
+
+/**
+ * class of gesture sensor
+ */
+class CGestureSensor
+{
+private:
+    gesture_sensor_handle_t m_gest_sensor;
+
+    /**
+     * prevent copy construct
+     */
+    CGestureSensor(const CGestureSensor &);
+    CGestureSensor& operator = (const CGestureSensor &);
+public:
+    /**
+      * @brief  constructor of CGestureSensor
+      *
+      * @param  tps number of four touch sensors
+      * @param  thres_percent touchpad would be active if the sample value decay to thres_percent
+      * @param  filter_value filter time of touchpad
+      */
+    CGestureSensor(const touch_pad_t *tps, uint16_t thres_percent = 990, uint32_t filter_value = DEFAULT_FILTER_MS);
+    
+    ~CGestureSensor();
+
+    /**
+      * @brief  add callback function
+      *
+      * @param  cb
+      * @param  arg
+      *
+      * @return
+      *     - ESP_OK: succeed
+      *     - others: fail
+      */
+    esp_err_t add_cb(gesture_cb cb, void *arg);
 };
 
 #endif
--- components/touchpad/touchpad_obj.cpp
+++ components/touchpad/touchpad_obj.cpp
@@ -28,95 +28,131 @@
 #include "touchpad.h"
 #include "esp_log.h"
 
-#define DEFAULT_THRES_PERCENT   90
-#define DEFAULT_FILTER_MS       150
-
-touchpad::touchpad(touch_pad_t touch_pad_num, uint16_t threshold = DEFAULT_THRES_PERCENT, uint32_t filter_value = DEFAULT_FILTER_MS)
+CTouchPad::CTouchPad(touch_pad_t touch_pad_num, uint16_t thres_percent, uint32_t filter_value)
 {
-    m_tp_handle = touchpad_create(touch_pad_num, threshold, filter_value);
+    m_tp_handle = touchpad_create(touch_pad_num, thres_percent, filter_value);
 }
 
-touchpad::~touchpad()
+CTouchPad::~CTouchPad()
 {
     touchpad_delete(m_tp_handle);
     m_tp_handle = NULL;
 }
 
-esp_err_t touchpad::add_cb(touchpad_cb_type_t cb_type, touchpad_cb cb, void  *arg)
+esp_err_t CTouchPad::add_cb(touchpad_cb_type_t cb_type, touchpad_cb cb, void  *arg)
 {
     return touchpad_add_cb(m_tp_handle, cb_type, cb, arg);
 }
 
-esp_err_t touchpad::set_serial_trigger(uint32_t trigger_thres_sec, uint32_t interval_ms, touchpad_cb cb, void *arg)
+esp_err_t CTouchPad::set_serial_trigger(uint32_t trigger_thres_sec, uint32_t interval_ms, touchpad_cb cb, void *arg)
 {
     return touchpad_set_serial_trigger(m_tp_handle, trigger_thres_sec, interval_ms, cb, arg);
 }
 
-esp_err_t touchpad::add_custom_cb(uint32_t press_sec, touchpad_cb cb, void  *arg)
+esp_err_t CTouchPad::add_custom_cb(uint32_t press_sec, touchpad_cb cb, void  *arg)
 {
     return touchpad_add_custom_cb(m_tp_handle, press_sec, cb, arg);
 }
 
-touch_pad_t touchpad::touchpad_num()
+touch_pad_t CTouchPad::touchpad_num()
 {
     return touchpad_num_get(m_tp_handle);
 }
 
-esp_err_t touchpad::set_threshold(uint32_t threshold)
+esp_err_t CTouchPad::set_threshold(uint32_t threshold)
 {
     return touchpad_set_threshold(m_tp_handle, threshold);
 }
 
-esp_err_t touchpad::set_filter(uint32_t filter_value)
+esp_err_t CTouchPad::set_filter(uint32_t filter_value)
 {
     return touchpad_set_filter(m_tp_handle, filter_value);
 }
 
-uint16_t touchpad::value()
+uint16_t CTouchPad::value()
 {
-    uint16_t tp_value;
+    uint16_t tp_value = 0;
     touchpad_read(m_tp_handle, &tp_value);
     return tp_value;
 }
 
-touchpad_slide::touchpad_slide(uint8_t num, const touch_pad_t *tps, uint32_t pos_scale = 2, uint16_t thres_percent = DEFAULT_THRES_PERCENT, uint32_t filter_value = DEFAULT_FILTER_MS)
+CTouchPadSlide::CTouchPadSlide(uint8_t num, const touch_pad_t *tps, uint32_t pos_scale, uint16_t thres_percent, uint32_t filter_value)
 {
     m_tp_slide_handle = touchpad_slide_create(num, tps, pos_scale, thres_percent, filter_value);
 }
 
-touchpad_slide::~touchpad_slide()
+CTouchPadSlide::~CTouchPadSlide()
 {
     touchpad_slide_delete(m_tp_slide_handle);
     m_tp_slide_handle = NULL;
 }
 
-uint8_t touchpad_slide::get_position()
+uint8_t CTouchPadSlide::get_position()
 {
     return touchpad_slide_position(m_tp_slide_handle);
 }
 
-touchpad_matrix::touchpad_matrix(uint8_t x_num, uint8_t y_num, const touch_pad_t *x_tps, const touch_pad_t *y_tps, uint16_t thres_percent = DEFAULT_THRES_PERCENT, uint32_t filter_value = DEFAULT_FILTER_MS)
+CTouchPadMatrix::CTouchPadMatrix(uint8_t x_num, uint8_t y_num, const touch_pad_t *x_tps, const touch_pad_t *y_tps, uint16_t thres_percent, uint32_t filter_value)
 {
     m_tp_matrix = touchpad_matrix_create(x_num, y_num, x_tps, y_tps, thres_percent, filter_value);
 }
 
-touchpad_matrix::~touchpad_matrix()
+CTouchPadMatrix::~CTouchPadMatrix()
 {
     touchpad_matrix_delete(m_tp_matrix);
     m_tp_matrix = NULL;
 }
 
-esp_err_t touchpad_matrix::add_cb(touchpad_cb_type_t cb_type, touchpad_matrix_cb cb, void *arg)
+esp_err_t CTouchPadMatrix::add_cb(touchpad_cb_type_t cb_type, touchpad_matrix_cb cb, void *arg)
 {
     return touchpad_matrix_add_cb(m_tp_matrix, cb_type, cb, arg);
 }
 
-esp_err_t touchpad_matrix::add_custom_cb(uint32_t press_sec, touchpad_matrix_cb cb, void *arg)
+esp_err_t CTouchPadMatrix::add_custom_cb(uint32_t press_sec, touchpad_matrix_cb cb, void *arg)
 {
     return touchpad_matrix_add_custom_cb(m_tp_matrix, press_sec, cb, arg);
 }
 
-esp_err_t touchpad_matrix::set_serial_trigger(uint32_t trigger_thres_sec, uint32_t interval_ms, touchpad_matrix_cb cb, void *arg)
+esp_err_t CTouchPadMatrix::set_serial_trigger(uint32_t trigger_thres_sec, uint32_t interval_ms, touchpad_matrix_cb cb, void *arg)
 {
     return touchpad_matrix_set_serial_trigger(m_tp_matrix, trigger_thres_sec, interval_ms, cb, arg);
+}
+
+CProximitySensor::CProximitySensor(touch_pad_t tp, uint16_t thres_percent, uint32_t filter_value)
+{
+    m_prox_sensor = proximity_sensor_create(tp, thres_percent, filter_value);
+}
+
+CProximitySensor::~CProximitySensor()
+{
+    proximity_sensor_delete(m_prox_sensor);
+    m_prox_sensor = NULL;
+}
+
+esp_err_t CProximitySensor::add_cb(proximity_cb cb, void *arg)
+{
+    return proximity_sensor_add_cb(m_prox_sensor, cb, arg);
+}
+
+uint16_t CProximitySensor::value()
+{
+    uint16_t val = 0;
+    proximity_sensor_read(m_prox_sensor, &val);
+    return val;
+}
+
+CGestureSensor::CGestureSensor(const touch_pad_t *tps, uint16_t thres_percent, uint32_t filter_value)
+{
+    m_gest_sensor = gesture_sensor_create(tps, thres_percent, filter_value);
+}
+
+CGestureSensor::~CGestureSensor()
+{
+    gesture_sensor_delete(m_gest_sensor);
+    m_gest_sensor = NULL;
+}
+
+esp_err_t CGestureSensor::add_cb(gesture_cb cb, void *arg)
+{
+    return gesture_sensor_add_cb(m_gest_sensor, cb, arg);
 }
