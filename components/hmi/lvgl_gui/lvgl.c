10837124f7f54f0d4ba3080012b995f3036e068a
InfiniteYuan1 <infiniteyuan@outlook.com>
2018-12-28 15:13:09 +0800

    lvgl: change rtos timer to esp_timer

--- components/hmi/lvgl_gui/lvgl.c
+++ components/hmi/lvgl_gui/lvgl.c
@@ -19,19 +19,22 @@
 /* LVGL includes */
 #include "iot_lvgl.h"
 
-static void IRAM_ATTR lvgl_task_time_callback(TimerHandle_t xTimer)
-{
-    /* Periodically call this function.
-     * The timing is not critical but should be between 1..10 ms */
-    lv_task_handler();
-}
+// wait for execute lv_task_handler and lv_tick_inc to avoid some widget don't refresh.
+#define LVGL_INIT_DELAY 100 // unit ms
 
-static void IRAM_ATTR lv_tick_task_callback(TimerHandle_t xTimer)
+static void lv_tick_timercb(void *timer)
 {
     /* Initialize a Timer for 1 ms period and
      * in its interrupt call
      * lv_tick_inc(1); */
     lv_tick_inc(1);
+}
+
+static void lv_task_timercb(void *timer)
+{
+    /* Periodically call this function.
+     * The timing is not critical but should be between 1..10 ms */
+    lv_task_handler();
 }
 
 void lvgl_init()
@@ -42,33 +45,35 @@ void lvgl_init()
     /* Initialize LittlevGL */
     lv_init();
 
-    /* Initialize a Timer for 1 ms period and in its interrupt call */
-    TimerHandle_t lvgl_tick_timer = xTimerCreate(
-                                        "lv_tickinc_task",
-                                        1 / portTICK_PERIOD_MS, // period time
-                                        pdTRUE,                 // auto load
-                                        (void *)NULL,           // timer parameter
-                                        lv_tick_task_callback); // timer callback
-    xTimerStart(lvgl_tick_timer, 0);
+    esp_timer_create_args_t timer_conf = {
+        .callback = lv_tick_timercb,
+        .name     = "lv_tick_timer"
+    };
+
+    esp_timer_handle_t g_wifi_connect_timer = NULL;
+    esp_timer_create(&timer_conf, &g_wifi_connect_timer);
+
+    esp_timer_start_periodic(g_wifi_connect_timer, 1 * 1000U);
 
     /* Display interface */
-    lvgl_lcd_display_init(); /* Initialize your display */
+    lvgl_lcd_display_init(); /*Initialize your display*/
 
 #if defined(CONFIG_LVGL_DRIVER_TOUCH_SCREEN_ENABLE) || defined(CONFIG_LVGL_DRIVER_TOGGLE_ENABLE)
     /* Input device interface */
-    lv_indev_drv_t indevdrv = lvgl_indev_init(); /* Initialize your indev */
+    lv_indev_drv_t indevdrv = lvgl_indev_init(); /*Initialize your indev*/
 #endif
 
-    /* lv_task_handler should be called periodically around 10ms */
-    TimerHandle_t lvgl_timer = xTimerCreate(
-                                   "lv_task",
-                                   10 / portTICK_PERIOD_MS,  // period time
-                                   pdTRUE,                   // auto load
-                                   (void *)NULL,             // timer parameter
-                                   lvgl_task_time_callback); // timer callback
-    xTimerStart(lvgl_timer, 0);
+    esp_timer_create_args_t lv_task_timer_conf = {
+        .callback = lv_task_timercb,
+        .name     = "lv_task_timer"
+    };
 
-    vTaskDelay(100 / portTICK_PERIOD_MS);
+    esp_timer_handle_t lv_task_timer = NULL;
+    esp_timer_create(&lv_task_timer_conf, &lv_task_timer);
+
+    esp_timer_start_periodic(lv_task_timer, 5 * 1000U);
+
+    vTaskDelay(LVGL_INIT_DELAY / portTICK_PERIOD_MS);    // wait for execute lv_task_handler and lv_tick_inc to avoid some widget don't refresh.
 
 #ifdef CONFIG_LVGL_DRIVER_TOUCH_SCREEN_ENABLE
     /* Calibrate touch screen */
