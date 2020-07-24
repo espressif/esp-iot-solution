eb4541e619d583ec1de7a46cf2fb1b09f714c83e
zhouli <zhouli@espressif.com>
2020-07-24 10:51:26 +0800

    bugfix(lcd): modify compile options to close ralated warnings

--- components/spi_devices/lcd/component.mk
+++ components/spi_devices/lcd/component.mk
@@ -6,3 +6,5 @@ CXXFLAGS += -D__AVR_ATtiny85__
 
 COMPONENT_ADD_INCLUDEDIRS := include Adafruit-GFX-Library/Fonts Adafruit-GFX-Library
 COMPONENT_SRCDIRS := . Adafruit-GFX-Library
+
+Adafruit-GFX-Library/glcdfont.o: CFLAGS += -Wno-unused-const-variable
