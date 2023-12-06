7ed0d7371d43cf42f239188a64e8ef6c19a4127d
Liu Han <liuhan@espressif.com>
2023-12-06 13:01:48 +0800

    feat(ble_hrp): Add heart rate profile and examples

---
+++ examples/bluetooth/ble_profiles/ble_hrp/main/app_hrp.h
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2019-2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int app_console_init(void);
+void register_hrp(void);
+
+#ifdef __cplusplus
+}
+#endif
