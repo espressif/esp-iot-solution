50f13a38529ec2e3ef8560ed6046031ccc6a6f9c
Li Bo <libo@espressif.com>
2024-11-04 21:08:35 +0800

    feat(ntc_driver): improve adc result using filter

--- components/sensors/ntc_driver/ntc_driver.c
+++ components/sensors/ntc_driver/ntc_driver.c
@@ -183,9 +183,51 @@ static esp_err_t ntc_voltage_to_temperature(ntc_device_handle_t ntc_handle, uint
     return ESP_FAIL;
 }
 
+#ifdef CONFIG_ENABLE_NTC_ADC_FILTER
+static esp_err_t adc_oneshot_get_filtered_result(adc_oneshot_unit_handle_t handle, adc_cali_handle_t cali_handle, adc_channel_t chan, int *filtered_result)
+{
+    NTC_DRIVER_CHECK(filtered_result != NULL, "Pointer of filtered_result is invalid", ESP_FAIL);
+    int vol[CONFIG_NTC_FILTER_WINDOW_SIZE] = {0};
+    int avg = 0;
+    for (int i = 0; i < sizeof(vol) / sizeof(vol[0]); i++) {
+        int raw = 0;
+        NTC_DRIVER_CHECK(adc_oneshot_read(handle, chan, &raw) == ESP_OK, "adc oneshot read fail", ESP_FAIL);
+        ESP_LOGV(TAG, "raw: %d", raw);
+        NTC_DRIVER_CHECK(adc_cali_raw_to_voltage(cali_handle, raw, &vol[i]) == ESP_OK, "adc calibration fail", ESP_FAIL);
+        ESP_LOGV(TAG, "vol: %d", vol[i]);
+        avg += vol[i];
+    }
+    // calculate the average value
+    avg /= sizeof(vol) / sizeof(vol[0]);
+    *filtered_result = avg;
+    // calculate the standard deviation
+    int std_vol = 0;
+    for (int i = 0; i < sizeof(vol) / sizeof(vol[0]); i++) {
+        std_vol += (vol[i] - avg) * (vol[i] - avg);
+    }
+    std_vol = (int)sqrt(std_vol / (sizeof(vol) / sizeof(vol[0])));
+    // filter the result, if the difference between the value and the average value is greater than the standard deviation, the value is discarded
+    int count = 0;
+    int filtered_vol = 0;
+    for (int i = 0; i < sizeof(vol) / sizeof(vol[0]); i++) {
+        if (abs(vol[i] - avg) < std_vol) {
+            count++;
+            filtered_vol += vol[i];
+        } else {
+            ESP_LOGV(TAG, "filtered out: %d", vol[i]);
+        }
+    }
+    if (count > 0) {
+        *filtered_result = filtered_vol / count;
+    }
+
+    return ESP_OK;
+}
+#endif
+
 esp_err_t ntc_dev_get_temperature(ntc_device_handle_t ntc_handle, float *temperature)
 {
-    int voltage, adc_raw = 0;
+    int voltage = 0;
 
     if (ntc_handle == NULL) {
         ESP_LOGW(TAG, "Pointer of handle is invalid");
@@ -193,11 +235,15 @@ esp_err_t ntc_dev_get_temperature(ntc_device_handle_t ntc_handle, float *tempera
     }
 
     ntc_driver_dev_t *ndd = (ntc_driver_dev_t *)ntc_handle;
+#ifdef CONFIG_ENABLE_NTC_ADC_FILTER
+    esp_err_t ret = adc_oneshot_get_filtered_result(ndd->adc_handle, ndd->adc_cali_handle, ndd->s_ntc_config.channel, &voltage);
+#else
+    int adc_raw = 0;
     esp_err_t ret = adc_oneshot_read(ndd->adc_handle, ndd->s_ntc_config.channel, &adc_raw);
     NTC_DRIVER_CHECK(ret == ESP_OK, "adc oneshot read fail", ESP_FAIL);
     NTC_DRIVER_CHECK(adc_raw != 0, "adc raw data equal to 0", ESP_FAIL);
-
     ret = adc_cali_raw_to_voltage(ndd->adc_cali_handle, adc_raw, &voltage);
+#endif
     NTC_DRIVER_CHECK(ret == ESP_OK, "adc calibration raw to voltage fail", ESP_FAIL);
     NTC_DRIVER_CHECK(voltage != 0, "voltage equal to 0", ESP_FAIL);
     ntc_voltage_to_temperature(ndd, voltage, temperature);
