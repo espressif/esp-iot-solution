34393bbd36c6cc6cb886623da343746e710f57ce
yanke <yanke2@espressif.com>
2025-02-25 17:56:00 +0800

    feat(ai): add basic touch driver

---
+++ examples/ai/esp_dl/touchpad_digit_recognition/main/main.cpp
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <stdio.h>
+
+extern "C" void app_main(void)
+{
+
+}
