f7da6869bc5aa0dbd47dd87a1809b6061075ef71
Li Bo <libo@espressif.com>
2025-03-11 15:06:51 +0800

    docs: update chatbot configuration

--- docs/_static/js/chatbot_widget.js
+++ docs/_static/js/chatbot_widget.js
@@ -2,8 +2,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     var script = document.createElement("script");
     script.src = "https://widget.kapa.ai/kapa-widget.bundle.js";
-    script.setAttribute("data-website-id", "9746d4c8-20ab-44f2-8b1d-1aef4a92f8ac");
-    script.setAttribute("data-project-name", "ESP Test Tools and Guidelines");
+    script.setAttribute("data-website-id", "4f47d0c5-ab75-4808-877d-eeb96559cd84");
+    script.setAttribute("data-project-name", "ESP-IoT-Solution");
     script.setAttribute("data-project-color", "#C62817");
     script.setAttribute("data-project-logo", "https://dl.espressif.com/public/logo.png");
     script.setAttribute("data-button-image", "https://dl.espressif.com/chatbot/Chatbot.png");
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
     script.setAttribute("data-button-box-shadow", "0px 6px 12px 1px rgba(0,0,0,0.16)");
     script.setAttribute("data-modal-override-open-class", "test-ai");
     script.setAttribute("data-modal-disclaimer", "This custom large language model (LLM), trained on official documentation from espressif.com, is designed to provide technical support and answers related to Espressif’s products and services. Give it a try, share your thoughts, and let us know your feedback—we truly appreciate it! \n\n**Note**: AI-generated information may be incomplete or inaccurate. Always verify critical information with official sources.");
-    script.setAttribute("data-modal-example-questions", "What is EspRFTestTool Toolkit?,What is Flash Download Tool?");
+    script.setAttribute("data-modal-example-questions", "How to add button to my project?,Does ESP32-S3 support USB Camera?");
     script.async = true;
     document.head.appendChild(script);
   });
